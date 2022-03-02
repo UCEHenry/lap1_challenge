5f75c60a0ea41d036e068c7c3fb86d3acb0c8108
AlinaLauraV <vizitiu.alinalaura@yahoo.it>
2022-03-02 14:48:47 +0000

    added event listener function

---
+++ client/assets/script.js
@@ -0,0 +1,12 @@
+// SETUP
+const btn = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+// Bind event listeners
+btn.addEventListener('click', event =>{
+    console.log("hello")
+});
+
+button2.addEventListener('click', event =>{
+    console.log ("hello")
+});
