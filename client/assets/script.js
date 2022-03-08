73dc97a4c820e592e5593ab4dc5ea520cfa82c39
Henry <uce.henrys@gmail.com>
2022-03-08 00:15:54 +0000

    added the barebones to get the search results and all results showing on the front-end with the back-end

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,23 +1,44 @@
 // SETUP
+const form = document.querySelector('form');
 const btn1 = document.getElementById('button1');
-const btn2 = document.getElementById('button2');
 
 // Bind event listeners
+form.addEventListener('submit', searchResults);
 btn1.addEventListener('click', getAllResults);
-btn2.addEventListener('click', getOneResult);
+
+function searchResults(e){
+    e.preventDefault();
+    const cohort = e.target.input.value;
+    fetch(`http://localhost:3000/data/cohort/${cohort}`)
+        .then(res => res.json())
+        .then(res => {
+            let newList = document.createElement('li');
+            let addLink = document.createElement('a');
+            addLink.href = `${res.url}`;
+            addLink.textContent = res.name;
+            newList.appendChild(addLink);
+            document.querySelector("#results").append(newList);
+        });
+};
 
 function getAllResults(e){
     e.preventDefault()
     fetch('http://localhost:3000/data') 
     .then(res => res.json())
     .then(res => {
-            let data = [];
+            // let data = [];
             res.forEach(element => {
-                console.log(element.name);
-                data.push(element.name);
+                // console.log(element.name);
+                // data.push(element.name);
+                let newList = document.createElement('li');
+                let addLink = document.createElement('a');
+                addLink.href = `${element.url}`;
+                addLink.textContent = element.name;
+                newList.appendChild(addLink);
+                document.querySelector("#results").append(newList);
             });
-            console.log(data);
-            document.querySelector("#result").innerHTML = data;
+            // console.log(data);
+            // document.querySelector("#result").innerHTML = data;
 
         });
         // .then(res => {
@@ -26,43 +47,3 @@ function getAllResults(e){
         //     console.log(data);
         // })
 };
-
-// function redirect(event) {
-//     event.preventDefault();
-//     fetch(`http://localhost:3000/data`)
-//       .then(resp => resp.text())
-//       .then(function(text){
-//         if(text === "No result found"){
-//           document.getElementById('log').innerHTML = text
-//         }else{
-//           window.location.replace(text);
-//         }
-//       }
-//       )
-    
-//   }
-
-function getOneResult(){
-    fetch('http://localhost:3000/data')
-        .then(res => res.json())
-        .then(data => {
-            let resources = data;
-            console.log(data);
-
-            resources.map(function(resource) {
-                let li = document.createElement("ul")
-                let name = document.createElement("li");
-                name.innerHTML = `${resource.name}`;
-                li.appendChild(name);
-            });
-        });
-        ul.appendChild(li);
-};
-
-// function getOneResult(){
-//     fetch('http://localhost:3000/data')
-//         .then(res => res.text())
-//         .then(findByName => document.getElementById('result').innerHTML = findByName)
-        
-// }
-
